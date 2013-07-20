589df813b64e8be6ffe5b5f1faef1ac338ee4be2
Tarek Saier <tareksaier@gmail.com>
2013-07-20 20:24:39 +0200

    improved wlanffs.sh

--- .scripts/wlanffs.sh
+++ .scripts/wlanffs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-while [ `sudo netctl status $1 | grep -Po 'Active: failed' | wc -c` -eq "15" ]
+while [ `sudo netctl status $1 | grep -Po 'Active: active' | wc -c` -ne "15" ]
 do
 echo "failed"
 sudo netctl start $1
