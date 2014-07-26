8823c81ff3bfd82fe2968ad9e7cbcfa522524f4c
Tarek Saier <tareksaier@gmail.com>
2014-07-26 16:38:57 +0200

    improved wallpaper script setbg.sh

--- .scripts/screen_tft24and22_rotated.sh
+++ .scripts/screen_tft24and22_rotated.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 xrandr --output LVDS --off
-xrandr --output HDMI-0 --preferred
-xrandr --output VGA-0 --preferred --rotate right --left-of HDMI-0
+xrandr --output HDMI-0 --preferred --primary
+xrandr --output VGA-0 --preferred --rotate right --right-of HDMI-0
--- .scripts/setbg.sh
+++ .scripts/setbg.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-l=${#1}
-if [ "$l" -lt "1" ]
-then
-find ~/eigenedaten/bilder/wallpaper/1920x1080/set -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |shuf -n1 -z | xargs -0 feh --bg-scale
-else
-find ~/eigenedaten/bilder/wallpaper/$1 -type f \( -name '*.jpg' -o -name '*.png' \) -print0 |shuf -n1 -z | xargs -0 feh --bg-scale
-fi
+
+screen_lines=`xrandr | grep -P "connected [^(]"`
+screen_count=`echo "$screen_lines" | wc -l`
+files=""
+
+for (( i=1; i<=$screen_count; i++ ))
+do
+screen_line=`echo "$screen_lines" | head -n $i | tail -n 1`
+screen_res=`echo "$screen_line" | grep -Po "\d+x\d+"`
+image=`find "/home/tarek/eigenedaten/bilder/wallpaper/$screen_res/" -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n1 -z`
+files="$files $image"
+done
+
+feh --bg-scale $files
+
+# i presume this'll fail when xrandr ouput order doesn't match screen order. but it does for me right now so — meh.
